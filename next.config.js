e7662c0290bb06b6928e28766f8024412b2dbabd
Dispatch <dispatch@cartercodes.dev>
2026-04-08 21:01:12 -0400

    Site deployment

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/lsb-gastonia-dental-group-gastonia',
+  images: { unoptimized: true },
+};
+module.exports = nextConfig;
